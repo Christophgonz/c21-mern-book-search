06b2ef2b6400bf10d8616a26c53d4084e8d39dd3
cgonz <christoph.gonz@gmail.com>
2023-08-05 22:48:12 -0700

    working on apollo server connection

--- server/server.js
+++ server/server.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const path = require('path');
 const db = require('./config/connection');
-const routes = require('./routes');
 const { ApolloServer } = require('apollo-server-express');
-const { typeDefs, resolvers } = require('./Schemas');
+const { typeDefs, resolvers } = require('./schemas');
 const { authMiddleware } = require('./utils/auth');
+const routes = require('./routes');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -23,6 +23,10 @@ if (process.env.NODE_ENV === 'production') {
 }
 
 app.use(routes);
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+});
 
 const startApolloServer = async () => {
   await server.start();
